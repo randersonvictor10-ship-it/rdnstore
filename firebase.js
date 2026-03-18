f3885ad20fb72ac56d3ba892046fd758a7c8f36a
randersonvictor10-ship-it <randersonvictor10@gmail.com>
2026-03-18 00:33:47 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, push, set, onValue, update, remove } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAkOwU1T92cz1stmo1-VwcXE_nIDp-sk-M",
   authDomain: "rdn-store.firebaseapp.com",
   databaseURL: "https://rdn-store-default-rtdb.firebaseio.com",
   projectId: "rdn-store",
-  storageBucket: "rdn-store.firebasestorage.app",
+  storageBucket: "rdn-store.appspot.com",
   messagingSenderId: "29377893404",
   appId: "1:29377893404:web:927e49e5f97c0f2368bbd9",
   measurementId: "G-CQWRHTJB29"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+export { db, ref, push, set, onValue, update, remove };
